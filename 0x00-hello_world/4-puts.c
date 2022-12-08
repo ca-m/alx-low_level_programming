fa219af9a7288bbba8ded0457b45a10cb4db238a
ca-m <deconnick.carmel@gmail.com>
2022-12-08 01:16:40 -0800

    try 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints a line of code using puts
+ * description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
