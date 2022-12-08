68ccdd6fa49e3a2e7c744cbebc16b58e47a66137
ca-m <deconnick.carmel@gmail.com>
2022-12-08 05:15:09 -0800

    5.1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
-/*
- * main - print a string followed by a new line
- * description: use function printf
- * return: 0
+/**
+ * main - entry
+ * Return: 0
  */
-
 int main(void)
 {
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
