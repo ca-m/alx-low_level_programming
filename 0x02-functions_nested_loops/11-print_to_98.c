dc129adbbbd1c4acc5c35a486fc8261667fcaafb
ca-m <deconnick.carmel@gmail.com>
2022-12-13 13:04:18 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,26 @@
 
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n > 98)
 	{
-		while (n > 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", n);
-			n++;
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
 	}
-	else if (n > 98)
+	else if (n >= 98)
 	{
-		while (n < 98)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%d, ", n);
-			n--;
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	printf("98\n");
 }
