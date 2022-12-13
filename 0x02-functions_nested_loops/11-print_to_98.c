7f7dba4f729ac4a44e5a03095bc3140e7d8531c6
ca-m <deconnick.carmel@gmail.com>
2022-12-13 12:55:20 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,13 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from input to 98, separated by a ccomma, in chronological order
+ * print_to_98 - prints numbers to 98
  * @n: number to being counting from
+ *
  * Description: print all natural numbers from n to 98
  * Return: void
  */
+
 void print_to_98(int n)
 {
 	if (n > 98)
