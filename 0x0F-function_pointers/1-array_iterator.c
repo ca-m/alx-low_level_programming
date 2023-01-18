3f6eea3a9fd41fe3acb981e804ff032846ff13fb
ca-m <deconnick.carmel@gmail.com>
2023-01-18 05:25:36 -0800

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,15 +10,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
-	if (array && actio)
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
 	{
-		i = 0;
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
+		action(*array);
+		array++;
 	}
 }
