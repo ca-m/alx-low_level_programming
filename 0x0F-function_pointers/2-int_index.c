2d282c7f6289e11794b78400eb630c8795575885
ca-m <deconnick.carmel@gmail.com>
2023-01-18 06:08:14 -0800

    task 2.1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,20 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
+	int i = 0;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	for (index = 0; index < size; index++)
+	if (size > 0)
 	{
-		if (cmp(array[index])
-			return (index);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
 	}
 
 	return (-1);
