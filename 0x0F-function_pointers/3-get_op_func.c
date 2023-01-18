4375e2eeee02ab7ce8a815faea6bd7e6da6d4130
ca-m <deconnick.carmel@gmail.com>
2023-01-18 06:50:56 -0800

    3.3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,26 +7,20 @@
  * @s: operator passed as argument
  * Return: pointer to corresponding function
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (0);
+	return (ops[i].f);
 }
