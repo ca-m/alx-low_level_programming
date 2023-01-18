4bea430a3a9886e3b90e51e860486ff1c3ac5391
ca-m <deconnick.carmel@gmail.com>
2023-01-18 06:41:56 -0800

    3.2

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,6 @@
  * @b: second number
  * Return: sum of a and b
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -20,7 +19,6 @@ int op_add(int a, int b)
  * @b: second number
  * Return: difference of a and b
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
@@ -32,7 +30,6 @@ int op_sub(int a, int b)
  * @b: second number
  * Return: product of a and b
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
@@ -44,7 +41,6 @@ int op_mul(int a, int b)
  * @b: second numer
  * Return: quotient of a and b
  */
-
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -62,7 +58,6 @@ int op_div(int a, int b)
  * @b: second number
  * Return: remainder of division of a by b
  */
-
 int op_mod(int a, int b)
 {
 	if (b == 0)
