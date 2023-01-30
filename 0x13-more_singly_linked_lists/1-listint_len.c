de70911c314c914e63b0324ea32ef48c762d22de
ca-m <deconnick.carmel@gmail.com>
2023-01-30 01:14:25 -0800

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in linked list
+ * @h: head pointer
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+			count++;
+	}
+	return (count);
+}
