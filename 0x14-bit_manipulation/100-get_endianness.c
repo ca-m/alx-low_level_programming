28e3968bb74e41d9156b4fe2e93ca618473432a9
ca-m <117754457+ca-m@users.noreply.github.com>
2023-02-02 12:23:00 +0200

    task 6.1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,16 @@
-#include "main.h"
-
-/**
- * get_endianness - checks endianness
- * Return: if big endian - 0, if litte endian - 1
- */
-
-int get_endianness(void)
-{
-	int num = 1;
-	char *endian = (char *)&num;
-
-	if (*endian = 1)
-		return (1);
-
-	return (0);
-} 
+#include "main.h" 
+ /** 
+  * get_endianness - checks the endianness 
+  * 
+  * Return: 0 if big endian, 1 if little endian 
+  */ 
+ int get_endianness(void) 
+ { 
+         int i; 
+         char *c; 
+  
+         i = 1; 
+         c = (char *)&i; 
+  
+         return (*c); 
+ }
